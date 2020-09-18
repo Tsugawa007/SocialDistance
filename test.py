ed60f3d61caf6f6b83d7d0c40c22086e6444dbfc
Ubuntu <ubuntu@openvino.o0q3o2nqrequhhcxkc2uiogotf.lx.internal.cloudapp.net>
2020-09-18 17:25:12 +0000

    update the comment

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ INPUT = './test.jpg'
 MODEL_WIDTH  = 544
 MODEL_HEIGHT = 320
 WINDOWS_TITLE = 'title'
+CONFIDENCE_RATIO = 0.6
 
 ie = IECore()
 net_path = ie.read_network(model=model_path+'.xml',weights=model_path+'.bin')
@@ -21,11 +22,12 @@ exec_net = ie.load_network(net_path,DEVICE)
  
 frame = cv2.imread(INPUT)
 frame = cv2.resize(frame,(MODEL_WIDTH,MODEL_HEIGHT))
-out= frame.transpose((2,0,1))
-out = np.expand_dims(out,axis=0)
-out = exec_net.infer(inputs={'data':out})
-out = out['detection_out']
-out = np.squeeze(out).astype('float')
+#pre-processing for opencv
+out= frame.transpose((2,0,1)) #HWC to CHW
+out = np.expand_dims(out,axis=0) #add the batch dim
+out = exec_net.infer(inputs={'data':out}) #run OpenVino Inference Engine
+out = out['detection_out'] # eextract elements
+out = np.squeeze(out).astype('float') #remove single-dim
 
 SIMILAR = 1.3
 SAFETY_DIST = 1.5 
@@ -35,14 +37,14 @@ def violate(detections,similar,safe_dist):
     violate = []
     for detection in detections:
         # confidence = detection[2]
-        if detection[2] >0.6:
+        if detection[2] >CONFIDENCE_RATIO:
             a = [(detection[5]-detection[3])/2+detection[3],(detection[6]-detection[4])/2+detection[4]] 
             w = np.abs([detection[5]-detection[3]])
             centorids.append(a)
             width.append(w)
     centorids = np.array(centorids)
     width = np.array(width)
-    width_sup = width/width.T
+    width_sup = width/width.T #calculate the rate between widths
     for i,row in enumerate(width_sup):
         for j,col in enumerate(row):
             if j<=i:
@@ -65,12 +67,11 @@ ALTER_COLOR = (0,0,255) #BGR
 SAFE_COLOR  = (0,255,0) 
 THICKNESS = 2
 for i,detection in enumerate(out):
-    confidence = detection[2]
-    if confidence > 0.6:
-        x_min = int(detection[3]*544)
-        y_min = int(detection[4]*320)
-        x_max = int(detection[5]*544)
-        y_max = int(detection[6]*320)
+    if detection[2] > CONFIDENCE_RATIO:
+        x_min = int(detection[3]*MODEL_WIDTH)
+        y_min = int(detection[4]*MODEL_HEIGHT)
+        x_max = int(detection[5]*MODEL_WIDTH)
+        y_max = int(detection[6]*MODEL_HEIGHT)
         if i in violate:
             color = ALTER_COLOR
         else:
